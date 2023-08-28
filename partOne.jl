ae5ea0dbcba509413577656b483bb45b03131bca
Zealand Maximo <zealandmaximo@Zealands-MacBook-Pro-2.local>
2023-08-28 18:20:22 -0400

    first little sudo code

---
+++ partOne.jl
@@ -0,0 +1,15 @@
+# Input from user
+c_1 = #coordinate one
+c_2 = #coordinate two
+c_3 = #coordinate three
+c_4 = #coordinate 4
+
+# for now, im initiating an array of zeros where time would go in 
+# the dimentions should be determined by how the input is given
+times = zeroes(Float64, 2, 4)
+# get following lines of times and assign them to an time array/list
+
+# Speed of light (m/s)
+c = 299792458
+
+
